7c16ee4b1974fff68dbffc0e8795a709242a3eff
kevinmbongo <81380383+kevinmbongo@users.noreply.github.com>
2021-12-03 19:14:51 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    'prettier',
+    'prettier/vue',
+  ],
+  rules: {},
+}
