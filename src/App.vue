83d78ac7fd2f6fc361a1ad7415d543c9dbf4f13f
kevinm <mbongo.kevin.pro@gmail.com>
2021-12-12 18:55:29 +0100

    chore(project): finish project

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
-  <div id="app">Hello World</div>
+  <div className="flex justify-center items-center bg-blue-400 h-screen w-full">
+    <img
+      className="w-1/2"
+      :src="require(`./assets/images/${imageSrc}.png`)"
+      alt="Ball"
+      @click="ballChange"
+    />
+  </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data: () => ({
+    imageSrc: 'ball1',
+  }),
+  methods: {
+    ballChange() {
+      const ballList = ['ball1', 'ball2', 'ball3', 'ball4', 'ball5']
+      this.imageSrc = ballList[Math.floor(Math.random() * ballList.length)]
+    },
+  },
 }
 </script>
